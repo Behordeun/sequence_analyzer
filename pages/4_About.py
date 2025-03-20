89e1a01114a19fa3d3aa11e225f9da4a0d6a0035
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-03-21 00:03:24 +0100

    Add About page to provide application overview and features

---
+++ pages/4_About.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("ℹ️ About This App")
+st.markdown(
+    """
+This Open-Source Sequencing Application was developed using:
+- **Python**
+- **Streamlit**
+- **Biopython**
+- **Clustal Omega**
+- **Plotly**
+
+### 🛠 Features:
+- **Sequence Alignment**
+- **DNA Analysis**
+- **RNA Analysis**
+- **GenBank Sequence Retrieval**
+"""
+)
