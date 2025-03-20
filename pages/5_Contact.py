dbaef62cf7b9613e67a598b37a20d0d80e085fe1
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-03-21 00:03:30 +0100

    Add Contact page for inquiries and contributions

---
+++ pages/5_Contact.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("📞 Contact")
+st.markdown(
+    """
+For any inquiries or contributions, please reach out:
+- 📧 Email: **bioinformatics@opensource.com**
+- 🛠 GitHub: **[Open-Source Sequencing Project](https://github.com/bioinformatics-project)**
+"""
+)
