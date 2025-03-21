78367a6d228d81d3a325cb9f5735b98814bbd2fd
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-03-21 04:03:15 +0100

    Add Contact page with inquiry and contribution information

---
+++ pages/5_Contact.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title("📞 Contact")
+st.markdown(
+    """
+For any inquiries or contributions, please reach out:
+- 📧 Email: **[abiodun.msulaiman@gmail.com](mailto:abiodun.msulaiman@gmail.com), [bolajioyeyemi@gmail.com](mailto:bolajioyeyemi@gmail.com)**
+- 🛠 GitHub: **[Sequence Analyzer](https://github.com/Behordeun/sequence_analyzer)**
+"""
+)
+
+
+# Footer
+st.markdown("---")  # Divider
+st.markdown(
+    """
+    **🔖 Sequence Analyzer Application**  
+    Developed with ❤️ using **Streamlit, Biopython, and Plotly by [Behordeun](https://github.com/Behordeun) and [Bollergene](https://github.com/bollergene)**  
+    📅 Version: 1.0.0 | 🔗 [GitHub Repository](https://github.com/Behordeun/sequence_analyzer)
+    
+    """,
+    unsafe_allow_html=True,
+)
